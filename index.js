07f4d8623af026dc20d6367ddb15dcda0fafeb05
kartik-patel-8085 <trainings@leons-group.com>
2025-01-16 18:32:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const { resolve } = require('path');
-let { post } = require('./models/post.model');
-let { sequelize } = require('./lib/index');
-const { read } = require('fs');
+const express = require("express");
+const { resolve } = require("path");
+let { post } = require("./models/post.model");
+let { sequelize } = require("./lib/index");
+const { read } = require("fs");
 
 const app = express();
 const port = 3000;
@@ -10,73 +10,73 @@ app.use(express.json());
 
 let posts = [
   {
-    title: 'Getting Started with Node.js',
+    title: "Getting Started with Node.js",
     content:
-      'This post will guide you through the basics of Node.js and how to set up a Node.js project.',
-    author: 'Alice Smith',
+      "This post will guide you through the basics of Node.js and how to set up a Node.js project.",
+    author: "Alice Smith",
   },
   {
-    title: 'Advanced Express.js Techniques',
+    title: "Advanced Express.js Techniques",
     content:
-      'Learn advanced techniques and best practices for building applications with Express.js.',
-    author: 'Bob Johnson',
+      "Learn advanced techniques and best practices for building applications with Express.js.",
+    author: "Bob Johnson",
   },
   {
-    title: 'ORM with Sequelize',
+    title: "ORM with Sequelize",
     content:
-      'An introduction to using Sequelize as an ORM for Node.js applications.',
-    author: 'Charlie Brown',
+      "An introduction to using Sequelize as an ORM for Node.js applications.",
+    author: "Charlie Brown",
   },
   {
-    title: 'Boost Your JavaScript Skills',
+    title: "Boost Your JavaScript Skills",
     content:
-      'A collection of useful tips and tricks to improve your JavaScript programming.',
-    author: 'Dana White',
+      "A collection of useful tips and tricks to improve your JavaScript programming.",
+    author: "Dana White",
   },
   {
-    title: 'Designing RESTful Services',
-    content: 'Guidelines and best practices for designing RESTful APIs.',
-    author: 'Evan Davis',
+    title: "Designing RESTful Services",
+    content: "Guidelines and best practices for designing RESTful APIs.",
+    author: "Evan Davis",
   },
   {
-    title: 'Mastering Asynchronous JavaScript',
+    title: "Mastering Asynchronous JavaScript",
     content:
-      'Understand the concepts and patterns for writing asynchronous code in JavaScript.',
-    author: 'Fiona Green',
+      "Understand the concepts and patterns for writing asynchronous code in JavaScript.",
+    author: "Fiona Green",
   },
   {
-    title: 'Modern Front-end Technologies',
+    title: "Modern Front-end Technologies",
     content:
-      'Explore the latest tools and frameworks for front-end development.',
-    author: 'George King',
+      "Explore the latest tools and frameworks for front-end development.",
+    author: "George King",
   },
   {
-    title: 'Advanced CSS Layouts',
-    content: 'Learn how to create complex layouts using CSS Grid and Flexbox.',
-    author: 'Hannah Lewis',
+    title: "Advanced CSS Layouts",
+    content: "Learn how to create complex layouts using CSS Grid and Flexbox.",
+    author: "Hannah Lewis",
   },
   {
-    title: 'Getting Started with React',
-    content: 'A beginners guide to building user interfaces with React.',
-    author: 'Ian Clark',
+    title: "Getting Started with React",
+    content: "A beginners guide to building user interfaces with React.",
+    author: "Ian Clark",
   },
   {
-    title: 'Writing Testable JavaScript Code',
+    title: "Writing Testable JavaScript Code",
     content:
-      'An introduction to unit testing and test-driven development in JavaScript.',
-    author: 'Jane Miller',
+      "An introduction to unit testing and test-driven development in JavaScript.",
+    author: "Jane Miller",
   },
 ];
 
-app.get('/seed_db', async (req, res) => {
+app.get("/seed_db", async (req, res) => {
   try {
     await sequelize.sync({ force: true });
     await post.bulkCreate(posts);
-    res.status(200).json({ message: 'Database Seeding Successful' });
+    res.status(200).json({ message: "Database Seeding Successful" });
   } catch (error) {
     res
       .status(500)
-      .json({ message: 'Error seeding database', error: error.message });
+      .json({ message: "Error seeding database", error: error.message });
   }
 });
 
@@ -86,11 +86,11 @@ async function fetchAllPosts() {
   return { posts };
 }
 
-app.get('/posts', async (req, res) => {
+app.get("/posts", async (req, res) => {
   try {
     let result = await fetchAllPosts();
     if (result.length === 0) {
-      return res.status(404).json({ message: 'Post not found' });
+      return res.status(404).json({ message: "Post not found" });
     }
     res.status(200).json(result);
   } catch (error) {
@@ -99,45 +99,49 @@ app.get('/posts', async (req, res) => {
 });
 
 // Exercise 2: Add a new post in the database
-async function addNewPost(postData) {
-  let newpost = await post.create(postData);
-  return { newpost };
+async function detailedPost(postId) {
+  return await post.findOne({ where: { id: postId } });
 }
-app.post('/posts/new', async (req, res) => {
-  try {
-    let newpost = req.body.newpost;
-    let response = await addNewPost(newpost);
 
-    return res.status(200).json(response);
+app.post("/posts/new", async (req, res) => {
+  try {
+    const { title, content, author } = req.body.newPost;
+
+    const newPost = await post.create({ title, content, author });
+
+    const postDetails = await detailedPost(newPost.id);
+
+    res.json({ newPost: postDetails });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
 // Exercise 3: Update post information
-async function updatePostById(id, newTrackData) {
-  let postDetails = await track.findOne({ where: { id } });
+async function updatePostById(id, newPostData) {
+  const [updatedCount, updatedPosts] = await post.update(newPostData, {
+    where: { id },
+    returning: true,
+  });
 
-  if (!trackDetails) {
-    return {};
+  if (updatedCount === 0) {
+    throw new Error("Post not found or no changes made");
   }
 
-  postDetails.set(newPostData);
-  let updatePost = await postDetails.save();
-  return { message: 'Post update successful', updatePost };
+  return updatedPosts[0];
 }
 
-app.post('/posts/update/:id', async (req, res) => {
+app.post("/posts/update/:id", async (req, res) => {
   try {
-    let newPostData = req.body;
-    let id = parseInt(req.params.id);
+    const id = req.params.id;
+    const newPostData = req.body;
 
-    let response = await updatePostById(id, newPostData);
+    const updatedPostData = await updatePostById(id, newPostData);
 
-    if (!response.message) {
-      return res.status(404).json({ message: 'No track found with this id' });
-    }
-    return res.status(200).json(response);
+    res.json({
+      message: "Post updated successfully",
+      updatedPost: updatedPostData,
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -145,19 +149,22 @@ app.post('/posts/update/:id', async (req, res) => {
 
 // Exercise 4: Delete a post from the database
 async function deletePostById(id) {
-  let deletePostById = await posts.destroy({ where: { id } });
-
-  if (deletePostById === 0) return {};
-  return { message: 'Track record Deleted' };
+  const result = await post.destroy({
+    where: { id },
+  });
+  return result;
 }
-app.post('/posts/delete', async (req, res) => {
+app.post("/posts/delete", async (req, res) => {
   try {
-    let id = parseInt(req.query.id);
-    let result = await deletePostById(id);
-    if (!result.length === 0) {
-      return res.status(404).json({ message: 'Post not found with this id' });
+    const { id } = req.body;
+
+    const deletionResult = await deletePostById(id);
+
+    if (deletionResult === 0) {
+      return res.status(404).json({ message: "Post not found" });
     }
-    return res.status(200).json(result);
+
+    res.json({ message: "Post record deleted successfully" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
